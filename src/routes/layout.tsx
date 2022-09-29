b05945d4c1fc39b320a44e210988b63a1cc833fd
haithem <haithem2001@gmail.com>
2022-09-29 22:01:53 +0100

    e

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,4 +1,5 @@
 import { component$, Slot, useStore } from "@builder.io/qwik";
+import Madeusing from "~/components/madeusing";
 import Header from "../components/header/header";
 
 export default component$(() => {
@@ -8,27 +9,23 @@ export default component$(() => {
 
   return (
     <div
-      class="h-screen w-screen overflow-auto"
+      class="relative h-screen w-screen overflow-auto"
       onScroll$={(e) => {
         console.log(e.view?.scrollY);
       }}
     >
       <div
-        class="fixed w-screen h-screen"
+        class="fixed -z-10 w-screen h-screen"
         style={{
           background: `linear-gradient(
             rgba(0, 0, 20, 0.8),
             rgba(0, 0, 0, 0.7)
-        ),  url(https://steamcdn-a.akamaihd.net/steamcommunity/public/images/items/619150/3bb00cbe95127f2fe36c85a33cfe69f4e5911234.jpg)`,
+        ),  url(/bg.jpg)`,
         }}
       ></div>
-      <main class="min-h-screen">
-        <Header />
-        <main class="h-full">
-          <Slot />
-        </main>
-      </main>
-      <footer class="flex absolute bottom-0 w-fulla justify-evenly z-20 p-2 bg-black text-white"></footer>
+
+      <Header />
+      <Slot />
     </div>
   );
 });
